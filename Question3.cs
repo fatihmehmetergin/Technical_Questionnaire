2aae4c540afb5fe5da45da83fbdbbb780c19c5d0
fatihmehmetergin <38405979+fatihmehmetergin@users.noreply.github.com>
2019-07-12 15:08:54 +0300

    Add files via upload

---
+++ Question3.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int a=58,b=34,temp;
+		
+			temp=a;
+			a=b;
+			b=temp;
+		
+			// after swapping 
+			Console.WriteLine("a="+a);
+			Console.WriteLine("b="+b);
+	}
+}
