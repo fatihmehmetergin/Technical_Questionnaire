420240d3e4fbf9c3421cbeb263ce0e935dbcf928
fatihmehmetergin <38405979+fatihmehmetergin@users.noreply.github.com>
2019-07-14 03:16:58 +0300

    Update Question5.sql

--- Question5.sql
+++ Question5.sql
@@ -1,4 +1,7 @@
 SELECT username,emailaddress FROM i_users WHERE userId=(SELECT userId FROM(
 SELECT MAX(fatih),userId FROM (Select userId, Count(userId) AS fatih From i_user_login_logs Group By userId Having Count (userId) > 3)
-WHERE userId NOT IN (10255))
-)
+WHERE userId NOT IN (10255)))
+                     
+                     
+                     
+                     
